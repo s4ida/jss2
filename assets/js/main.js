573aa338927cada54ff25e4ed5e045844b0ae039
s4ida <sd88c9gvh@code.edu.az>
2023-10-22 16:30:21 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,100 @@
+// 1.
+// let a = 3;
+// let b = -7;
+// let c = 4;
+//  Diskriminanti hesablama
+// let discriminant = b * b - 4 * a * c;
+//  Kvadrat tenliyin koklerini hesablama
+// if (discriminant > 0) {
+//     let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+//     let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+//     console.log("İki heqiqi kök var:");
+//     console.log("Kök 1: " + root1);
+//     console.log("Kök 2: " + root2);
+// } else if (discriminant === 0) {
+//     var root = -b / (2 * a);
+//     console.log("Bir heqiqi kök var:");
+//     console.log("Kök: " + root);
+// } else {
+//     console.log("Heqiqi kök yoxdur, qarisiq kökleri  var.");
+// }
+
+
+
+//2.
+// let a = 13; 
+// for (let i = 1; i <= 1000; i++) {
+//     if (i % a === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// 3.
+// const arr = [2,4,5,9,1]
+// const min = Math.min(...arr)
+// console.log(min)
+
+
+
+// 4.
+// const arr = [2,4,5,9,1]
+// const max = Math.max(...arr)
+// console.log(max)
+
+
+
+// 5.
+// const arr = [2, 4, 5, 9, 1];
+// const min = Math.min(...arr);
+
+// const index = arr.indexOf(min);
+// console.log(index);
+
+
+
+
+// 6.
+// const arr = [2, 4, 5, 9, 1];
+// const max = Math.max(...arr);
+
+// const index = arr.indexOf(max);
+// console.log(index);
+
+
+
+// 7.
+// const arr = [2, 4, 5, 9, 1];
+// let sum = 0;
+
+// for (let i = 1; i < arr.length; i+=2) {
+//     sum += arr[i];
+// }
+// console.log("cemi:",sum);
+
+
+
+// 8.
+// let text = "Mən Code Academy Tələbəsiyəm.";
+// let result = text.toLowerCase();
+// console.log(result);
+
+
+
+// 9.
+// let text = "            Mən Code Academydə Programing tədrisi alıram            ";
+// let result = text.trim();
+// console.log(result);
+
+
+// 10.
+// let text = "Mən Code Academy Tələbəsiyəm";
+// const myArray = text.split(" ");
+// console.log(myArray);
+
+// 11.
+// let text =  "            Mən Code Academydə Programing tədrisi alıram            ";
+// console.log(text.length);
+
+
+
